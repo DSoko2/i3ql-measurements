a70a3a81a7565598185b8c1d2e2d2a1f5d725501
Daniel Sokolowski <github@d.sokolowski.org>
2019-02-14 14:39:01 +0100

    Updated and centralized group labels

---
+++ evaluation/group-labels.R
@@ -0,0 +1,19 @@
+groupLabels <<- tibble( # Labels of the test series
+  group = c("ta001", "ta002", "ta003", "ta004", "ta005", "ta006", "ta007", "ta008",
+            "0004", "0005", "0006", "0007", "0008", "0009", "0010", "0011", "0012", "0013", "0014",
+            "0020",
+            "0021", "0022", "0023", "0024", "0025", "0026", "0027", "0028",
+            "0031", "0032", "0033", "0034", "0035", "0036", "0037", "0038",
+            "0041", "0042", "0046", "0047",
+            "0051", "0052", "0056", "0057"),
+  groupLabel = c("Product: SecQL", "Product: HL", "Product: HL & OL", "Product: HL & OL & Sel", "Sum: SecQL", "Sum: HL", "Sum: HL & OL", "Sum: HL & OL & Sel",
+                 "Client Only", "Privacy Disabled", "Privacy Aware", "SecQL NoPriv", "SecQL PrivAware", "Product NoPriv", "Product PrivAware", "Sum NoPriv", "Sum PrivAware", "Selectivity Sum NoPriv", "Selectivity Sum PrivAware",
+                 "Client Only",  # 0020, ...
+                 "0SecQL (NoPriv, Prod)", "1HL (NoPriv, Prod)", "2HL+OL (NoPriv, Prod)", "3HL+OL+Sel (NoPriv, Prod)",# 0021, ...
+                 "0SecQL (NoPriv, Sum)", "1HL (NoPriv, Sum)", "2HL+OL (NoPriv, Sum)", "3HL+OL+Sel (NoPriv, Sum)",
+                 "0SecQL (PrivAw, Prod)", "1HL (PrivAw, Prod)", "2HL+OL (PrivAw, Prod)", "3HL+OL+Sel (PrivAw, Prod)", # 0031, ...
+                 "0SecQL (PrivAw, Sum)", "1HL (PrivAw, Sum)", "2HL+OL (PrivAw, Sum)", "3HL+OL+Sel (PrivAw, Sum)",
+                 "4HL+OL+EvSel (NoPriv, Prod)", "5SecQL+EvSel (NoPriv, Prod)", # 0054, ...
+                 "4HL+OL+EvSel (NoPriv, Sum)", "5SecQL+EvSel (NoPriv, Sum)",
+                 "4HL+OL+EvSel (PrivAw, Prod)", "5SecQL+EvSel (PrivAw, Prod)", # 0051, ...
+                 "4HL+OL+EvSel (PrivAw, Sum)", "5SecQL+EvSel (PrivAw, Sum)"))
