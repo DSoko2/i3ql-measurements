9e726c801f62d241e434fa96c8f4005736858d25
Daniel Sokolowski <github@d.sokolowski.org>
2019-03-13 22:52:13 +0100

    Updated group labels

--- evaluation/group-labels.R
+++ evaluation/group-labels.R
@@ -5,7 +5,13 @@ groupLabels <<- tibble( # Labels of the test series
             "0021", "0022", "0023", "0024", "0025", "0026", "0027", "0028",
             "0031", "0032", "0033", "0034", "0035", "0036", "0037", "0038",
             "0041", "0042", "0046", "0047",
-            "0051", "0052", "0056", "0057"),
+            "0051", "0052", "0056", "0057",
+            
+            "0060",
+            "0061", "0062", "0063", "0064",
+            "0065", "0066", "0067", "0068",
+            "0071", "0072", "0073", "0074",
+            "0075", "0076", "0077", "0078"),
   groupLabel = c("Product: SecQL", "Product: HL", "Product: HL & OL", "Product: HL & OL & Sel", "Sum: SecQL", "Sum: HL", "Sum: HL & OL", "Sum: HL & OL & Sel",
                  "Client Only", "Privacy Disabled", "Privacy Aware", "SecQL NoPriv", "SecQL PrivAware", "Product NoPriv", "Product PrivAware", "Sum NoPriv", "Sum PrivAware", "Selectivity Sum NoPriv", "Selectivity Sum PrivAware",
                  "Client Only",  # 0020, ...
@@ -16,4 +22,11 @@ groupLabels <<- tibble( # Labels of the test series
                  "4HL+OL+EvSel (NoPriv, Prod)", "5SecQL+EvSel (NoPriv, Prod)", # 0054, ...
                  "4HL+OL+EvSel (NoPriv, Sum)", "5SecQL+EvSel (NoPriv, Sum)",
                  "4HL+OL+EvSel (PrivAw, Prod)", "5SecQL+EvSel (PrivAw, Prod)", # 0051, ...
-                 "4HL+OL+EvSel (PrivAw, Sum)", "5SecQL+EvSel (PrivAw, Sum)"))
+                 "4HL+OL+EvSel (PrivAw, Sum)", "5SecQL+EvSel (PrivAw, Sum)",
+                 
+                 "Client Only",
+                 
+                 "SecQL (NoPriv, Prod)", "SecQL+EvSel (NoPriv, Prod)", "HL+OL (NoPriv, Prod)", "HL+OL+EvSel (NoPriv, Prod)",
+                 "SecQL (NoPriv, Sum)", "SecQL+EvSel (NoPriv, Sum)", "HL+OL (NoPriv, Sum)", "HL+OL+EvSel (NoPriv, Sum)",
+                 "SecQL (PrivAw, Prod)", "SecQL+EvSel (PrivAw, Prod)", "HL+OL (PrivAw, Prod)", "HL+OL+EvSel (PrivAw, Prod)",
+                 "SecQL (PrivAw, Sum)", "SecQL+EvSel (PrivAw, Sum)", "HL+OL (PrivAw, Sum)", "HL+OL+EvSel (PrivAw, Sum)"))
